88529c174215e28bbf64dc2be8b81bd34be2e42b
fxym1573 <113717364+fxym1573@users.noreply.github.com>
2026-06-13 13:38:07 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#添加软件源
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 # 修改默认IP
  sed -i 's/192.168.1.1/192.168.0.254/g' package/base-files/files/bin/config_generate
 
