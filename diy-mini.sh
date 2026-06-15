e7b314df11c66bb183a7539ee9bb68a6cc4976d4
fxym1573 <113717364+fxym1573@users.noreply.github.com>
2026-06-15 21:49:33 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -34,9 +34,9 @@ git_sparse_clone main https://github.com/sirpdboy/luci-app-ddns-go.git package/d
 git_sparse_clone master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # Themes
-# git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-# git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-# git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 晶晨宝盒
 git_sparse_clone main https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
