1ca4100beb80e1e7229c3b66f923a298d0b90af0
fxym1573 <113717364+fxym1573@users.noreply.github.com>
2026-06-17 14:03:03 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -31,12 +31,12 @@ git_sparse_clone() {
 
 
 # 移除要替换的包
-rm -rf feeds/luci/applications/luci-app-openclash
+# rm -rf feeds/luci/applications/luci-app-openclash
 # rm -rf feeds/luci/applications/luci-app-dockerman
 
-#git_sparse_clone main https://github.com/sirpdboy/luci-app-ddns-go.git package/luci-app-ddns-go
-#git_sparse_clone main https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
-#git_sparse_clone master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+# git_sparse_clone main https://github.com/sirpdboy/luci-app-ddns-go.git package/luci-app-ddns-go
+# git_sparse_clone main https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+# git_sparse_clone master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
