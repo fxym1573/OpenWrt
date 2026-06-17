d0254236901818c263f1e98cf2e95e2f6679e133
fxym1573 <113717364+fxym1573@users.noreply.github.com>
2026-06-17 14:03:32 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -40,8 +40,8 @@ git_sparse_clone() {
 
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+# git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 晶晨宝盒
 git_sparse_clone main https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
